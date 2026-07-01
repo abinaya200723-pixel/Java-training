c23c72866008f0632f29823cf502e695d637cbba
abinaya200723-pixel <abinaya.2007.23@gmail.com>
2026-07-01 19:50:40 +0530

    Initial

---
+++ Day1/Problem12.java
@@ -0,0 +1,10 @@
+package Day1;
+
+class Problem12 {
+    static{
+        System.out.println("Static Block ");
+    }
+    public static void main(String[] args) {
+        System.out.println("Main Method");
+    }
+}
