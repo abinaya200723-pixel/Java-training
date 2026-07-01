b7ea2ea2f9aa31ae2617dc2f32abd26b9887badc
abinaya200723-pixel <abinaya.2007.23@gmail.com>
2026-07-01 19:45:10 +0530

    Initial

---
+++ Day1/Problem10.java
@@ -0,0 +1,11 @@
+package Day1;
+
+class Problem10 {
+    void show() {
+        System.out.println("method Called");
+    }
+    public static void main(String[] args) {
+        new MethodDemo().show();
+    }
+    
+}
