69bfdb6c3ae704ca01d13d5e9e6b3c77540d0c0e
abinaya200723-pixel <abinaya.2007.23@gmail.com>
2026-07-01 19:48:40 +0530

    Initial

---
+++ Day1/Problem11.java
@@ -0,0 +1,11 @@
+package Day1;
+
+ class Problem11 {
+    Problem11(){
+        System.out.println("Constructor Called");
+    }
+    public static void main(String[] args) {
+        new Problem11();
+    }
+    
+}
