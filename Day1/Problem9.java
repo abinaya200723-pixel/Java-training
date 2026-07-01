171e02b4586687bec4812af443f760545e90b5b1
abinaya200723-pixel <abinaya.2007.23@gmail.com>
2026-07-01 12:08:01 +0530

    Initial

---
+++ Day1/Problem9.java
@@ -0,0 +1,10 @@
+package Day1;
+
+ class Problem9 {
+    public static void main(String[] args) {
+        String name = "harini";
+        Problem9 p = new Problem9();
+        System.out.println(p.name );
+    }
+    
+}
